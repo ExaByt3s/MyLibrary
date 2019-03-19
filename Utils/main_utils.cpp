39237b826a16c57db5a1986d7e3c7cf839ded39a
yunate <yunate@foxmail.com>
2019-03-19 13:43:19 +0800

    编码转换

--- Utils/main_utils.cpp
+++ Utils/main_utils.cpp
@@ -6,17 +6,20 @@
 #include "MemoryShare/Test.h"
 #include "Json/Test.h"
 #include "XML/Test.h"
+#include "CodeCvt/Test.h"
 
 namespace utilstest {}
 
 int main()
 {
-	utilstest::Test_stringutils0();
-	utilstest::Test_stringutils1();
-	utilstest::Test_MemoryShare0();
-	utilstest::Test_Sql0();
-	utilstest::Test_Json();
-	utilstest::Test_xml0();
+// 	utilstest::Test_stringutils0();
+// 	utilstest::Test_stringutils1();
+// 	utilstest::Test_MemoryShare0();
+// 	utilstest::Test_Sql0();
+// 	utilstest::Test_Json();
+// 	utilstest::Test_xml0();
+
+	utilstest::Test_codecvt0();
 
 	return 0;
 }
